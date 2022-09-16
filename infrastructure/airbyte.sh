442097899de56fbea7671d349e750f4ff04d4136
krishnendu <corporate.krishnendu@gmail.com>
2022-09-16 11:46:25 +1000

    intalling airbyte

---
+++ infrastructure/airbyte.sh
@@ -0,0 +1,24 @@
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo yum install docker-compose
+sudo yum install git 
+git clone https://github.com/airbytehq/airbyte.git
+cd airbyte
+docker compose up -d
+
+
+
